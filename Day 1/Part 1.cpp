dad92aaff8c19e12a95b451658526761fa2b8450
Joeru-san <gioxel.vicario@gmail.com>
2021-12-01 12:17:16 +0100

    Restyling code and repository structure

---
+++ Day 1/Part 1.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    
+    int x = 0, y = 0, c = 0;
+    
+    in >> x;
+    
+    for(int i = 0; i < 2000; i++) {
+        in >> y;
+        
+        if(y > x) c++;
+
+        x = y;
+    }
+    
+    out << c;
+}
---
+++ Day 1/Part 2.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    
+    int x = 0, y = 0, z = 0, c = 0, tmp1 = 0, tmp2 = 0;
+
+    in >> x >> y >> z;
+
+    for(int i = 0; i < 1997; i++) { // 1997 we already took 3 input variables before the for cycle
+        tmp1 = x + y + z;
+        
+        x = y;                          
+        y = z;
+        in >> z;
+
+        tmp2 = x + y + z;
+
+        if(tmp1 < tmp2) c++;
+    }
+    
+    out << c;
+}
