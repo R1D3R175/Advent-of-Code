a655d68d99d880a3d519c7d36a542fbb9873a3ef
R1D3R175 <r1.d3.r175@gmail.com>
2021-12-02 14:45:46 +0100

    Created algorithm to solve the 2nd part of the 2nd day

---
+++ Day 2/Part 2.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    string s;
+    int i;
+
+    int x = 0, y = 0, a = 0;   
+
+    while(in >> s >> i) {
+        if(s == "forward") {
+            x += i;
+            y += a * i;
+        } else if(s == "up") 
+            a -= i;
+        else
+            a += i;
+        
+    }
+
+    out << x * y;
+
+    in.close();
+    out.close();
+}
