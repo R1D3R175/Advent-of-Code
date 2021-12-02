f98e1863ecb1064422923d765a3b8c1fce3e5adc
R1D3R175 <r1.d3.r175@gmail.com>
2021-12-02 14:37:31 +0100

    Created .gitignore Created algorithm for first part of second day

---
+++ Day 2/Part 1.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    string s;
+    int i;
+
+    int x = 0, y = 0;   
+
+    while(in >> s >> i) {
+        if(s == "forward")
+            x += i;
+        else if(s == "up") 
+            y -= i;
+        else
+            y += i;
+    }
+
+    out << x * y;
+
+    in.close();
+    out.close();
+}
