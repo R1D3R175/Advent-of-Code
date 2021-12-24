e3ea6e938270c82616a7e2d1b58c9cad4be985d9
R1D3R175 <r1.d3.r175@gmail.com>
2021-12-24 23:25:29 +0100

    Re-formatted Code

--- Day 1/Part 2.cpp
+++ Day 1/Part 2.cpp
@@ -1,28 +1,31 @@
 #include <fstream>
 using namespace std;
 
-int main(){
+int main()
+{
     ifstream in("input.txt");
     ofstream out("output.txt");
-    
+
     int x = 0, y = 0, z = 0, c = 0, tmp1 = 0, tmp2 = 0;
 
     in >> x >> y >> z;
 
-    for(int i = 0; i < 1997; i++) { // 1997 because we already took 3 input variables before the for cycle
+    for (int i = 0; i < 1997; i++)
+    { // 1997 because we already took 3 input variables before the for cycle
         tmp1 = x + y + z;
-        
-        x = y;                          
+
+        x = y;
         y = z;
         in >> z;
 
         tmp2 = x + y + z;
 
-        if(tmp1 < tmp2) c++;
+        if (tmp1 < tmp2)
+            c++;
     }
-    
+
     out << c;
-    
+
     in.close();
     out.close();
 }
