c51d0dd6f83472fcd5c5d941770b929cf6e8c5dc
R1D3R175 <r1.d3.r175@gmail.com>
2021-12-25 00:19:11 +0100

    Added newlines at the start of every cout

--- debug_print.h
+++ debug_print.h
@@ -3,24 +3,24 @@
 template <typename T, typename K>
 void print_pair(std::pair<T, K> const &p)
 {
-    std::cout << "[DEBUG] print_pair " << p.first << " - " << p.second << std::endl
+    std::cout << "\n[DEBUG] print_pair " << p.first << " - " << p.second << std::endl
               << std::endl;
 }
 
 template <typename T>
 void print_vec(std::vector<T> const &v)
 {
-    std::cout << "<--------------- [DEBUG] print_vec --------------->\n";
+    std::cout << "\n<--------------- [DEBUG] print_vec --------------->\n";
     for (auto const &a : v)
         std::cout << a << " ";
 
-    std::cout << "<--------------- [DEBUG] print_vec --------------->\n\n";
+    std::cout << "\n<--------------- [DEBUG] print_vec --------------->\n\n";
 }
 
 template <typename T>
 void print_mat(std::vector<std::vector<T>> const &m)
 {
-    std::cout << "<--------------- [DEBUG] print_mat --------------->\n";
+    std::cout << "\n<--------------- [DEBUG] print_mat --------------->\n";
     for (std::vector<T> const &v : m)
     {
         for (auto const &a : v)
@@ -29,5 +29,5 @@ void print_mat(std::vector<std::vector<T>> const &m)
         std::cout << std::endl;
     }
 
-    std::cout << "<--------------- [DEBUG] print_mat --------------->\n\n";
+    std::cout << "\n<--------------- [DEBUG] print_mat --------------->\n\n";
 }
